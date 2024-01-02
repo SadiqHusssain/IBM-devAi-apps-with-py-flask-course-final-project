3c0e098dc05e71ff39119234e79f8d6394a6fff2
SadiqHusssain <sadiqhussain17647@gmail.com>
2024-01-02 16:47:10 -0500

    done statc code analysis on server.py till 10/10

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,6 +4,7 @@ import json
 import requests
 
 def emotion_detector(text_to_analyse):
+    """this function talks to the Watson ai library for sentiment analysis"""
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     raw_document= {"raw_document": {"text": text_to_analyse}}
@@ -13,7 +14,6 @@ def emotion_detector(text_to_analyse):
     if status == 400:
         emotions = {'anger':None, 'sadness':None, 'fear':None, 'joy':None, 'disgust':None, 'dominant_emotion':None}
         return emotions
-
     emotions =  formatted_response['emotionPredictions'][0]['emotion']
     dominant_emotion = [None, -100.0] # to compare scores and get name of dominant emotion.
     for emotion in emotions:
@@ -21,7 +21,7 @@ def emotion_detector(text_to_analyse):
             dominant_emotion = [emotion, emotions[emotion]]
     emotions['dominant_emotion'] = dominant_emotion[0]
     return emotions
-
+    
 
 
 
--- server.py
+++ server.py
@@ -1,26 +1,29 @@
-from flask import Flask, request, render_template, url_for, redirect
+""" scipt for web deployment of the emotion detection system"""
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection webApp")
 
 @app.route("/")
 def index():
+    """a simple function that render the index page"""
     return render_template("index.html")
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def emotion_detect():
+    """this function exchange data between app UI and emotion detection module."""
     text_to_analyse = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyse)
     # Extracting emotions and their values
-    emotions = ', '.join(f"'{emotion}': {value}" for emotion, value in result.items() if emotion != 'dominant_emotion')
+    emotions = ', '.join(f"'{emotion}': \
+    {value}" for emotion,value in result.items() if emotion != 'dominant_emotion')
     dominant_emotion = result['dominant_emotion']
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
     # Creating the statement
-    response = f"For the given statement, the system response is {emotions}. The dominant emotion is {dominant_emotion}."
+    response = f"For the given statement, the system response is {emotions}.\
+     The dominant emotion is {dominant_emotion}."
     return response
 
-
-    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
